90bce9c171233023823077f67234a96a2a1229aa
inzayn99 <arbaazkh4n@gmail.com>
2021-06-23 14:36:01 +0545

    NEWS Base Project

--- app/Http/Controllers/Backend/AdminUserController.php
+++ app/Http/Controllers/Backend/AdminUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Models\AdminUser\AdminUser;
 use Illuminate\Http\Request;
 
 class AdminUserController extends BackendController
@@ -14,11 +15,27 @@ class AdminUserController extends BackendController
     public function add(Request $request)
     {
         if ($request->isMethod('get')) {
-            return view($this->pagePath . '. admins.add-admin-user', $this->data);
+            return view($this->pagePath . '.admins.add-admin-user', $this->data);
 
         }
         if ($request->isMethod('post')) {
+            $this->validate($request, [
+                'name' => 'required|min:3|max:100',
+                'username' => 'required|min:3|max:20|unique:admin_users,username',
+                'email' => 'required|email|unique:admin_users,email',
+                'password' => 'required|min:3|max:20|confirmed',
+                'password_confirmation' => 'required',
+                'image' => 'mimes:jpg,jpeg,gif,png'
+            ]);
 
+            $data['name'] = $request->name;
+            $data['username'] = $request->username;
+            $data['email'] = $request->email;
+            $data['password'] = bcrypt($request->password);
+
+            if (AdminUser::created($data)) {
+                echo "success";
+            }
         }
     }
 }
--- resources/views/backend/pages/admins/add-admin-user.blade.php
+++ resources/views/backend/pages/admins/add-admin-user.blade.php
@@ -6,7 +6,6 @@
     <div class="right_col" role="main">
         <div class="">
 
-
             <div class="clearfix"></div>
 
             <div class="row">
@@ -18,7 +17,8 @@
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
                                 <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                       aria-expanded="false"><i class="fa fa-wrench"></i></a>
                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                         <a class="dropdown-item" href="#">Settings 1</a>
                                         <a class="dropdown-item" href="#">Settings 2</a>
@@ -32,7 +32,55 @@
                         <div class="x_content">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <h1>Add</h1>
+                                    <form action="{{route('add-admin-user')}}" method="post" enctype="multipart/form-data">
+                                        {{csrf_field()}}
+                                        <div class="form-group">
+                                            <label for="name">Name: <a
+                                                    style="color: red;">{{$errors->first('name')}}</a></label>
+                                            <input type="text" name="name" class="form-control form-control-sm"
+                                                   id="name" value="{{old('name')}}">
+
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="username">Username: <a
+                                                    style="color: red;">{{$errors->first('username')}}</a></label>
+                                            <input type="text" name="username" class="form-control form-control-sm"
+                                                   id="username" value="{{old('username')}}">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="email">Email: <a
+                                                    style="color: red;">{{$errors->first('email')}}</a></label>
+                                            <input type="text" name="email" class="form-control form-control-sm"
+                                                   id="email" value="{{old('email')}}">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="password">password: <a
+                                                    style="color: red;">{{$errors->first('password')}}</a></label>
+                                            <input type="password" name="password" class="form-control form-control-sm"
+                                                   id="password" value="{{old('password')}}">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="password_confirmation">Confirm Password: <a
+                                                    style="color: red;">{{$errors->first('password_confirmation')}}</a></label>
+                                            <input type="password" name="password_confirmation" class="form-control form-control-sm"
+                                                   id="password_confirmation" value="{{old('password_confirmation')}}">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label for="image">Image: <a
+                                                    style="color: red;">{{$errors->first('image')}}</a></label><br>
+                                            <input type="file" name="image" class="btn btn-default">
+                                        </div>
+
+                                        <div class="form-group">
+                                           <button class="btn btn-success">Add Record</button>
+                                        </div>
+
+
+                                    </form>
                                 </div>
                             </div>
                         </div>
